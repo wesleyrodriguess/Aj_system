553571708a4c19fc8cde70752d25adc4f18541e6
Wesley Rodrigues <wesleysilvarodrigues25@gmail.com>
2021-01-02 19:39:27 -0300

    created login authenticated and base template adm

--- src/AjSystem/AdminBundle/Controller/DefaultController.php
+++ src/AjSystem/AdminBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AjSystem\AdminBundle\Controller;
 
+use AjSystem\AdminBundle\Entity\Administrador;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class DefaultController
@@ -14,10 +17,33 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/", name="home")
+     * @Security("has_role('ROLE_ADMINISTRADOR')")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        return ;
+    }
+
+    /**
+     * @Route("/geraruser/adm")
+     */
+    public function gerarAction(Request $request)
+    {
+
+        $user =  new Administrador();
+
+        $password = $this->get('security.password_encoder')->encodePassword($user, 'admingrazielle');
+
+        $user->setNome("Grazielle");
+        $user->setPassword($password);
+        $user->setRoles("ROLE_ADMINISTRADOR");
+        $user->setEmail("grazielle_aj@gmail.com");
+        $user->setActive(true);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
         return ;
     }
 }
---
+++ web/js/adm.js
@@ -0,0 +1,4 @@
+// mostra text quando passar o mouse
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
